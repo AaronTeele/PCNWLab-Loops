e5cc58ae68b0acde30998d65946e252d1bec5cfa
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2018-10-21 11:46:50 -0400

    solved pset 2

--- pset2.js
+++ pset2.js
@@ -5,7 +5,14 @@
     @desc - takes all engative numbers and makes positive
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
+    const removeNegatives = arr => {
+        const arrCopy = [];
+        for(let i = 0; i < arr.length; i++){
+            arrCopy.push(Math.abs(arr[i]));
+        }; return arrCopy;
+    };
 
+    console.log(removeNegatives([-3,-2,1,2,3,-4])); // [3,2,1,2,3,4]
 /*
     @func findVal
     @param {array} arr
@@ -19,6 +26,17 @@
         findVal([1,2,3,4], 4); // 3
         findVal([1,2,3,4], 9); // -1
 */
+    const findVal = (arr, val) => {
+        for (let i = 0; i < arr.length; i++){
+            if (arr[i] === val) {
+                return i;
+            };            
+        }; return -1
+    };
+
+    console.log(findVal([1,2,3,4], 1)); // 0
+    console.log(findVal([1,2,3,4], 4)); // 3
+    console.log(findVal([1,2,3,4], 9)); // -1
 
 /*
     @func removeOdds
@@ -31,6 +49,18 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
+    const removeOdds = arr => {
+        const evens = [];
+    
+        for (let i = 0; i < arr.length; i++){
+            if (typeof arr[i] === "number" && arr[i] % 2 === 0) {
+                evens.push(arr[i]);
+            };
+        }; return evens;
+    };
+
+    console.log(removeOdds([1,2,3,4])); // [2,4]
+    console.log(removeOdds([1,"2",3,4])); // [4]
 
 /*
     @func addSquares
@@ -40,7 +70,15 @@
     @example - 
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
+    const addSquares = arr => {
+        const arrCopy = arr.slice();
+        for (let i = 0; i < arr.length; i++){
+            arrCopy.push(arrCopy[i] ** 2);
+        };
+        return arrCopy;
+    };
 
+    console.log(addSquares([1,2,3,4])); // [1,2,3,4,1,4,9,16])
 /*
     @func doubleify
     @param {array} arr
@@ -49,7 +87,30 @@
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
 
+    const doubleify = arr => {
+        let newArr = [...arr];
 
+            for(let i = 0; i < arr.length; i++){
+                newArr.splice(arr[i] + 2 , 0, arr[i]);
+            } return newArr;
+    }
+    console.log(doubleify([1,2,3,4])); // [1,1,2,2,3,3,4,4])
+
+
+    const doubleify = arr => {
+        const arrCopy = arr.slice();
+        // for (let i = 0; i < arr.length; i++){
+        //     for (let j = 0; j < 3; j++) {
+        //         arrCopy.push(arr[i]);
+        //     };
+        // };
+        for (let i = 0; i < arrCopy.length; i += 2){
+            arrCopy.splice(i, 0, arrCopy[i])
+        }
+        return arrCopy;
+        }
+
+    console.log(doubleify([1,2,3,4])); // [1,1,2,2,3,3,4,4])
 /*
     @func findMax
     @param {array}
@@ -57,3 +118,16 @@
     @desc - find largest number in array
     @example - findMax([1,2,3,99,4]); // 99 
 */
+
+    const findMax = arr => {
+        // let max = Math.max.apply(null, arr)
+        // return max;
+        let largest = arr[0];
+        for (let i = 0; i < arr.length; i++){
+            if(arr[i] > largest) {
+                largest = arr[i];
+            }; 
+        }; return largest;
+    }
+
+    console.log(findMax([1,2,3,99,4]));
